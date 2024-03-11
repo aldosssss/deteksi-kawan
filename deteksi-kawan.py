b82e08577875eb36c9f0e2ee3be37523c4f77404
Aldo Stepanus Kaban <133597774+aldosssss@users.noreply.github.com>
2024-03-11 17:10:57 +0700

    convert rgb2hsv

--- deteksi-kawan.py
+++ deteksi-kawan.py
@@ -1,12 +1,11 @@
 import cv2
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 while True:
     ret, frame = cap.read()
-    cv2.imshow('Webcam', frame)
+    HSV_Image=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+    cv2.imshow('Webcam', HSV_Image)
     
     if cv2.waitKey(1) == ord('q'):
         break
 cap.release()
 cv2.destroyAllWindows()
-aaaaaa
-aldo
