9b083aa52b3dcc4f29509fe1510aa5171ea02bc9
Aldo Stepanus Kaban <133597774+aldosssss@users.noreply.github.com>
2024-03-25 00:26:52 +0700

    tahap metode HSV

--- deteksi-kawan.py
+++ deteksi-kawan.py
@@ -15,11 +15,13 @@ def front():
     #cv2.imshow('original',img)
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     
-    lower_yellow = np.array ([69,79,149])
-    upper_yellow = np.array ([104,172,255])
+   # lower_cyan = np.array ([69,79,149]) #warna cyan
+    #upper_cyan = np.array ([104,172,255])
+    lower_magenta = np.array ([106,87,84]) #warna cyan
+    upper_magenta = np.array ([179,255,255])
     edges = cv2.Canny(img, 15, 125)
-
-    mask = cv2.inRange (hsv, lower_yellow, upper_yellow)
+    mask = cv2.inRange (hsv, lower_magenta, upper_magenta)
+    #mask = cv2.inRange (hsv, lower_cyan, upper_cyan)
     
     kernel = np.ones((5,5), np.float32)/255
     #median = cv2.medianBlur(res, 15)
@@ -28,32 +30,33 @@ def front():
 
     opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
     closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel)
+    img, edges = canny_edge_detection(closing)
 
     font=cv2.FONT_HERSHEY_COMPLEX
-    contours,_ = cv2.findContours(closing.copy(),cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+   # contours,_ = cv2.findContours(closing.copy(),cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
     
-    if len(contours)>0:
-        c = max(contours,key=cv2.contourArea)
-        ((x, y), radius) = cv2.minEnclosingCircle(c)
-        M = cv2.moments(c)
-        if M["m00"]==0:
-            M["m00"]=1
-        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-        x = int(M["m10"] / M["m00"])
-        y = int(M["m01"] / M["m00"])
-        if radius > 2:
-            cv2.circle(img,(10,20), 10, (0,0,255), -1)
-            cv2.putText(img, "D", (27, 25), font, 0.6, (0, 255, 0), 2)
-            cv2.circle(img, (int(x), int(y)), int(radius),(0, 255, 0), 2)
-            cv2.circle(img, (int(x), int(y)), 5, (0, 255, 255), -1)
+   # if len(contours)>0:
+    #    c = max(contours,key=cv2.contourArea)
+     #   ((x, y), radius) = cv2.minEnclosingCircle(c)
+      #  M = cv2.moments(c)
+       # if M["m00"]==0:
+        #    M["m00"]=1
+        #center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+       # x = int(M["m10"] / M["m00"])
+        #y = int(M["m01"] / M["m00"])
+        #if radius > 2:
+         #   cv2.circle(img,(10,20), 10, (0,0,255), -1)
+          #  cv2.putText(img, "D", (27, 25), font, 0.6, (0, 255, 0), 2)
+           # cv2.circle(img, (int(x), int(y)), int(radius),(0, 255, 0), 2)
+            #cv2.circle(img, (int(x), int(y)), 5, (0, 255, 255), -1)
             
-        else :
-            kamera = 'o'
-    else:
-        kamera = 'o'
+       # else :
+        #    kamera = 'o'
+    #else:
+    #    kamera = 'o' 
         
     cv2.imshow('closing',closing)
-    cv2.imshow('result',img)
+    cv2.imshow('result',edges)
 
 while True:
     front()
